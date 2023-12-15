55bf6f7be3300d5d23687fb15b6b223c79c8ceac
Michael Caesario <53443317+mikecaesario@users.noreply.github.com>
2023-12-15 23:24:38 +0700

    Initial Commit

---
+++ Project Health Check/ContentView.swift
@@ -0,0 +1,29 @@
+//  
+//  ContentView.swift
+//  Project Health Check
+//
+//  Created with ❤️‍🔥 by Michael Caesario on 15/12/23.
+//  Copyright © 2023 Michael Caesario. All rights reserved.
+// 
+//  Website: https://mikecaesario.app
+//  GitHub: https://github.com/mikecaesario
+//  LinkedIn: https://www.linkedin.com/in/mikecaesario/
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project Health Check/Project_Health_CheckApp.swift
@@ -0,0 +1,22 @@
+//  
+//  Project_Health_CheckApp.swift
+//  Project Health Check
+//
+//  Created with ❤️‍🔥 by Michael Caesario on 15/12/23.
+//  Copyright © 2023 Michael Caesario. All rights reserved.
+// 
+//  Website: https://mikecaesario.app
+//  GitHub: https://github.com/mikecaesario
+//  LinkedIn: https://www.linkedin.com/in/mikecaesario/
+//
+
+import SwiftUI
+
+@main
+struct Project_Health_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
