0948a60ff307c28d41194416e58a4659d85f9f34
Michael Caesario <53443317+mikecaesario@users.noreply.github.com>
2024-01-03 23:29:52 +0700

    added tab bar and 3 other screens: Dashboard View, Pill Tracker View, Mood Tracker View

---
+++ Project Health Check/Screens/MoodTrackerView.swift
@@ -0,0 +1,23 @@
+//  
+//  MoodTrackerView.swift
+//  Project Health Check
+//
+//  Created with ❤️‍🔥 by Michael Caesario on 03/01/24.
+//  Copyright © 2024 Michael Caesario. All rights reserved.
+// 
+//  Website: https://mikecaesario.app
+//  GitHub: https://github.com/mikecaesario
+//  LinkedIn: https://www.linkedin.com/in/mikecaesario/
+//
+
+import SwiftUI
+
+struct MoodTrackerView: View {
+    var body: some View {
+        Text("Mood Tracker View")
+    }
+}
+
+#Preview {
+    MoodTrackerView()
+}
