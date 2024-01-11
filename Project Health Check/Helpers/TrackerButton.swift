1ff825363de05e18a6f7999ccfa5d934f32339f0
James Ciaston <jimmyciaston@gmail.com>
2024-01-11 08:33:31 -0500

    --Added reusable Button struct for Main Views --Added PillTrackerView

---
+++ Project Health Check/Helpers/TrackerButton.swift
@@ -0,0 +1,18 @@
+//
+//  TrackerButton.swift
+//  Project Health Check
+//
+//  Created by Jim Ciaston on 1/11/24.
+//
+
+import SwiftUI
+
+struct TrackerButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrackerButton()
+}
